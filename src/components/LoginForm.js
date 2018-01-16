d8e1d0e9cfa9a3e5bff8cf7bf529616a3c2469f5
xinhezhang <xinhezhang@hotmail.com>
2018-01-16 03:36:10 -0800

    LoginForm UI ready

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
-      <Card />
+      <Card>
+        <CardSection>
+          <Input
+            label='Email'
+            placeholder='test@gmail.com'
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            label='Password'
+            placeholder='password'
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            login
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
