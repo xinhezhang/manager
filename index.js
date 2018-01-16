49197e61ac2361f4475065551f222aaf0b6f1e6b
xinhezhang <xinhezhang@hotmail.com>
2018-01-16 01:53:47 -0800

    setup starter code

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src/App';
 
-AppRegistry.registerComponent('manager', () => App);
+// get rid of “Remote debugger is in a background tab” warning
+console.ignoredYellowBox = ['Remote debugger'];
+
+AppRegistry.registerComponent('[Your Project Name]', () => App);
+
+// How to add app icon?
+//
+// https://blog.bam.tech/developper-news/change-your-react-native-app-icons-in-a-single-command-line
+//
+// npm install -g yo generator-rn-toolbox
+// brew install imagemagick
+// yo rn-toolbox:assets --icon <path to your icon>
