c3e83aa55119764e640cd93605834b01407bb4e4
xinhezhang <xinhezhang@hotmail.com>
2018-01-16 03:59:15 -0800

    connect action creator (emailChanged) with component (LoginForm)

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import { emailChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
+  onEmailChange = (text) => {
+    this.props.emailChanged(text);
+  }
+
   render() {
     return (
       <Card>
@@ -10,6 +16,7 @@ class LoginForm extends Component {
           <Input
             label='Email'
             placeholder='test@gmail.com'
+            onChangeText={this.onEmailChange}
           />
         </CardSection>
 
@@ -23,7 +30,7 @@ class LoginForm extends Component {
 
         <CardSection>
           <Button>
-            login
+            Login
           </Button>
         </CardSection>
       </Card>
@@ -31,4 +38,4 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+export default connect(null, { emailChanged })(LoginForm);
