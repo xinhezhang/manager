2a3bb5ab91087c802bcdf74173daf51608b59e8f
xinhezhang <xinhezhang@hotmail.com>
2018-01-16 22:46:29 -0800

    EmployeeCreate UI ready

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Create</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane Doe"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="(999)999-9999"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
