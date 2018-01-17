c4efcb3d7f662ea45c22172944a31bf7ce4f8264
xinhezhang <xinhezhang@hotmail.com>
2018-01-16 23:32:24 -0800

    implement name/phone input change

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+  onNameChange = (text) => {
+    this.props.employeeUpdate({ key: 'name', value: text });
+  }
+
+  onPhoneChange = (text) => {
+    this.props.employeeUpdate({ key: 'phone', value: text });
+  }
+
   render() {
     return (
       <Card>
@@ -11,6 +21,8 @@ class EmployeeCreate extends Component {
             autoCapitalize="words"
             label="Name"
             placeholder="Jane Doe"
+            value={this.props.name}
+            onChangeText={this.onNameChange}
           />
         </CardSection>
 
@@ -18,6 +30,8 @@ class EmployeeCreate extends Component {
           <Input
             label="Phone"
             placeholder="(999)999-9999"
+            value={this.props.phone}
+            onChangeText={this.onPhoneChange}
           />
         </CardSection>
 
@@ -31,4 +45,12 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, {
+  employeeUpdate,
+})(EmployeeCreate);
