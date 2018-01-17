a44003d5f4eda18d4289b0fe2c4ac1d57451c828
xinhezhang <xinhezhang@hotmail.com>
2018-01-16 23:31:43 -0800

    fixed error (change object to { key, value })

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -9,17 +9,15 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action);
-
   switch (action.type) {
     case EMPLOYEE_UPDATE:
       // // ES5 syntax
       // const newState = {};
-      // newState[action.payload.props] = action.payload.value;
+      // newState[action.payload.key] = action.payload.value;
       // return { ...state, newState };
 
-      // action.payload === { props: 'name', value: 'Jane Doe' }
-      return { ...state, [action.payload.props]: action.payload.value };
+      // action.payload === { key: 'name', value: 'Jane Doe' }
+      return { ...state, [action.payload.key]: action.payload.value };
     default:
       return state;
   }
