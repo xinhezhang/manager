7d3e281d4d6075b397b5193b7c00adf0346c4161
xinhezhang <xinhezhang@hotmail.com>
2018-01-17 03:40:13 -0800

    create a reusable form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 import {
   employeeUpdate,
   employeeCreate,
 } from '../actions';
-import { Card, CardSection, Input, Button } from './common';
+
+import { Card, CardSection, Button } from './common';
+import EmployeeForm from './EmployeeForm';
 
 class EmployeeCreate extends Component {
-  onNameChange = (text) => {
-    this.props.employeeUpdate({ key: 'name', value: text });
-  }
-
-  onPhoneChange = (text) => {
-    this.props.employeeUpdate({ key: 'phone', value: text });
-  }
-
-  onPickerChange = (day) => {
-    this.props.employeeUpdate({ key: 'shift', value: day });
-  }
-
   onButtonPress = () => {
     const { name, phone, shift } = this.props;
 
@@ -29,59 +18,12 @@ class EmployeeCreate extends Component {
     this.props.employeeCreate({ name, phone, shift });
   }
 
-  renderPickerItems = () => {
-    const weekdays = [
-      'None',
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-      'Sunday',
-    ];
-
-    return weekdays.map((day) => {
-      return <Picker.Item key={day} label={day} value={day} />;
-    });
-  }
-
   render() {
     console.log(this.props.employee);
 
     return (
       <Card>
-        <CardSection>
-          <Input
-            autoCapitalize="words"
-            label="Name"
-            placeholder="Jane Doe"
-            value={this.props.name}
-            onChangeText={this.onNameChange}
-          />
-        </CardSection>
-
-        <CardSection>
-          <Input
-            label="Phone"
-            placeholder="999-999-9999"
-            value={this.props.phone}
-            onChangeText={this.onPhoneChange}
-          />
-        </CardSection>
-
-        {/* override default style { flexDirection: 'row' } */}
-        <CardSection style={{ flexDirection: 'column' }}>
-          <Text style={styles.pickerLabelStyle}>
-            Shift
-          </Text>
-          <Picker
-            selectedValue={this.props.shift}
-            onValueChange={this.onPickerChange}
-          >
-            {this.renderPickerItems()}
-          </Picker>
-        </CardSection>
+        <EmployeeForm {...this.props} />
 
         <CardSection>
           <Button onPress={this.onButtonPress}>
@@ -92,14 +34,6 @@ class EmployeeCreate extends Component {
     );
   }
 }
-
-const styles = {
-  pickerLabelStyle: {
-    fontSize: 18,
-    fontWeight: '600',
-    padding: 5,
-  },
-};
 
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.employeeForm;
---
+++ src/components/EmployeeForm.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { View, Text, Picker } from 'react-native';
+import { connect } from 'react-redux';
+
+import {
+  employeeUpdate,
+  employeeCreate,
+} from '../actions';
+import { CardSection, Input } from './common';
+
+class EmployeeForm extends Component {
+  onNameChange = (text) => {
+    this.props.employeeUpdate({ key: 'name', value: text });
+  }
+
+  onPhoneChange = (text) => {
+    this.props.employeeUpdate({ key: 'phone', value: text });
+  }
+
+  onPickerChange = (day) => {
+    this.props.employeeUpdate({ key: 'shift', value: day });
+  }
+
+  renderPickerItems = () => {
+    const weekdays = [
+      'None',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday',
+    ];
+
+    return weekdays.map((day) => {
+      return <Picker.Item key={day} label={day} value={day} />;
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <CardSection>
+          <Input
+            autoCapitalize="words"
+            label="Name"
+            placeholder="Jane Doe"
+            value={this.props.name}
+            onChangeText={this.onNameChange}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="999-999-9999"
+            value={this.props.phone}
+            onChangeText={this.onPhoneChange}
+          />
+        </CardSection>
+
+        {/* override default style { flexDirection: 'row' } */}
+        <CardSection style={{ flexDirection: 'column' }}>
+          <Text style={styles.pickerLabelStyle}>
+            Shift
+          </Text>
+          <Picker
+            selectedValue={this.props.shift}
+            onValueChange={this.onPickerChange}
+          >
+            {this.renderPickerItems()}
+          </Picker>
+        </CardSection>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  pickerLabelStyle: {
+    fontSize: 18,
+    fontWeight: '600',
+    padding: 5,
+  },
+};
+
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, {
+  employeeUpdate,
+  employeeCreate,
+})(EmployeeForm);
