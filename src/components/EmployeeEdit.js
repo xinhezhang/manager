22ebcf5ace84dfe035b606125aeea58e96e08011
xinhezhang <xinhezhang@hotmail.com>
2018-01-17 03:46:55 -0800

    create EmployeeEdit component

---
+++ src/components/EmployeeEdit.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import {
+  employeeUpdate,
+} from '../actions';
+
+import { Card, CardSection, Button } from './common';
+import EmployeeForm from './EmployeeForm';
+
+class EmployeeEdit extends Component {
+  render() {
+    return (
+      <Card>
+        <EmployeeForm />
+
+        <CardSection>
+          <Button>
+            Save Changes
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default connect(null, {
+  employeeUpdate,
+})(EmployeeEdit);
