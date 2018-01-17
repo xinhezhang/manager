370dc60f632af5fac7b22738989078c7c49b0f75
xinhezhang <xinhezhang@hotmail.com>
2018-01-16 22:14:23 -0800

    setup initial Scene for navigation

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router>
       <Scene key="root">
-        <Scene key="login" component={LoginForm} title="Please Login" />
+        <Scene
+          initial
+          key="login"
+          component={LoginForm}
+          title="Please Login"
+        />
+
+        <Scene
+          key="employeeList"
+          component={EmployeeList}
+          title="Employee List"
+        />
       </Scene>
     </Router>
   );
