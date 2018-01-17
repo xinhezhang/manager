47afe609e83e7af95171995ade22bde5ffeb36e1
xinhezhang <xinhezhang@hotmail.com>
2018-01-16 23:07:39 -0800

    refactor code for action creators

---
+++ src/actions/AuthActions.js
@@ -0,0 +1,62 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  LOGIN_USER,
+} from './types';
+
+export const emailChanged = (text) => {
+  return {
+    type: EMAIL_CHANGED,
+    payload: text,
+  };
+};
+
+// action creator returns a object
+export const passwordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
+    payload: text,
+  };
+};
+
+// action creator returns a function (by using redux-thunk)
+// used for "Async" actions
+// dispatch: a function, it allows manually send action to all the reducers
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    dispatch({
+      type: LOGIN_USER,
+    });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((user) => loginUserSuccess(dispatch, user))
+      .catch((error) => {
+        console.log(error);
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then((user) => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
+      });
+  };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user,
+  });
+
+  // goto main (EmployeeList, EmployeeCreate, ...) page
+  Actions.main();
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({
+    type: LOGIN_USER_FAIL,
+  });
+};
