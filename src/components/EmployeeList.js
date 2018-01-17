daf6a5cecb16c64ada5b15ee3bb697f522ad82fb
xinhezhang <xinhezhang@hotmail.com>
2018-01-17 02:42:16 -0800

    build employee list dynamic and in time

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -8,13 +8,27 @@ class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
 
+    // we also need build list in here since when user add a new employee,
+    // then come back, this component will be re-rendered, and this life-cycle
+    // method will be called
+    this.createDataSource(this.props);
+  }
+
+  // "nextProps" are the next set of props that
+  // this component will be rendered with
+  // "this.props" is still the old set of props
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
     const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2
     });
 
     // "employees" from application state at 'reducers/index.js'
     // { employees: EmployeeReducer }
-    this.dataSource = ds.cloneWithRows(this.props.employees);
+    this.dataSource = ds.cloneWithRows(employees);
   }
 
   render() {
