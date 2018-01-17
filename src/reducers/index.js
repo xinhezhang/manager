0584645f931d09088fe8c4e3ebac841dcff5296c
xinhezhang <xinhezhang@hotmail.com>
2018-01-16 23:10:35 -0800

    link reducers for EmployeeFormReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
 import AuthReducer from './AuthReducer';
+import EmployeeFormReducer from './EmployeeFormReducer';
 
 // global application state
 export default combineReducers({
   auth: AuthReducer,
+  employeeForm: EmployeeFormReducer,
 });
