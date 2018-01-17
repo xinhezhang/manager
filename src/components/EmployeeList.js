5cf2d44827f65020abfe9637f5fca7239dba97bc
xinhezhang <xinhezhang@hotmail.com>
2018-01-17 02:17:08 -0800

    test fetching data from Firebase

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
+  }
+  
   render() {
     return (
       <View>
@@ -15,4 +22,4 @@ class EmployeeList extends Component {
   }
 }
 
-export default EmployeeList;
+export default connect(null, { employeesFetch })(EmployeeList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 
 import AuthReducer from './AuthReducer';
 import EmployeeFormReducer from './EmployeeFormReducer';
+import EmployeeReducer from './EmployeeReducer';
 
 // global application state
 export default combineReducers({
   auth: AuthReducer,
   employeeForm: EmployeeFormReducer,
+  employees: EmployeeReducer,
 });
