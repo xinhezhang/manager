1b5c347fb34b62f4dc79d6564d274b723756c7bb
xinhezhang <xinhezhang@hotmail.com>
2018-01-17 02:23:40 -0800

    store data by Firebase uid

--- src/reducers/EmployeeReducer.js
+++ src/reducers/EmployeeReducer.js
@@ -7,8 +7,13 @@ const INITIAL_STATE = {};
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMPLOYEES_FETCH_SUCCESS:
-      // TODO
-      return state;
+      // action.payload ===
+      // {
+      //   uid1: { name: "Jane Doe", phone: "999-999-9999", shift: "Monday" },
+      //   uid2: { ... },
+      //   ...
+      // }
+      return action.payload;
     default:
       return state;
   }
